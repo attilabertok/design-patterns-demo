05dbc915533488339eabe957dec939699fa7151e
Attila Bertok <attila.bertok@zeiss.com>
2022-08-22 15:05:33 +0200

    Rx subject based implementation of Observer.HeadFirst

---
+++ Observer/Observer.HeadFirst.Rx/Program.cs
@@ -0,0 +1,22 @@
+﻿using Observer.HeadFirst.Common.Messages;
+using Observer.HeadFirst.Rx.Displays;
+
+namespace Observer.HeadFirst.Rx
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var weatherData = new WeatherData();
+            var _ = new List<IObserver<WeatherChangeMessage>>
+            {
+                new CurrentConditions(weatherData),
+                new Forecast(weatherData),
+                new HeatIndex(weatherData),
+                new Statistics(weatherData)
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Observer/Observer.HeadFirst.Rx/WeatherData.cs
@@ -0,0 +1,27 @@
+﻿using System.Reactive.Subjects;
+using Observer.HeadFirst.Common.Messages;
+using Observer.HeadFirst.Common.WeatherStation;
+
+namespace Observer.HeadFirst.Rx;
+
+public class WeatherData :
+    WeatherDataBase,
+    IObservable<WeatherChangeMessage>
+{
+    private readonly Subject<WeatherChangeMessage> subject;
+
+    public WeatherData()
+    {
+        subject = new Subject<WeatherChangeMessage>();
+    }
+
+    public override void MeasurementsChanged()
+    {
+        subject.OnNext(new WeatherChangeMessage(Temperature, Humidity, Pressure));
+    }
+
+    public IDisposable Subscribe(IObserver<WeatherChangeMessage> observer)
+    {
+        return subject.Subscribe(observer);
+    }
+}
