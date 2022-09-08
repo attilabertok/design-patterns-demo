56976669f4d3297b04ce64119f951f3885207246
bat <hun.azazel@gmail.com>
2022-09-08 10:48:23 +0200

    Decorator.HeadFirst.Io

---
+++ Decorator/Decorator.HeadFirst.Io/LowerCaseInputStream.cs
@@ -0,0 +1,59 @@
+﻿namespace Decorator.HeadFirst.Io;
+
+public class LowerCaseInputStream :
+    Stream
+{
+    private readonly Stream stream;
+
+    public LowerCaseInputStream(Stream stream)
+    {
+        this.stream = stream;
+    }
+
+    public override void Flush()
+    {
+        stream.Flush();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        var bytesRead = stream.Read(buffer, offset, count);
+        for (var i = offset; i < offset + bytesRead; i++)
+        {
+            var originalCharacter = Convert.ToChar(buffer[i]);
+            var lowerCaseCharacter = char.ToLower(originalCharacter);
+            buffer[i] = Convert.ToByte(lowerCaseCharacter);
+        }
+
+        return bytesRead;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return stream.Seek(offset, origin);
+    }
+
+    public override void SetLength(long value)
+    {
+        stream.SetLength(value);
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        stream.Write(buffer, offset, count);
+    }
+
+    public override bool CanRead => stream.CanRead;
+
+    public override bool CanSeek => stream.CanSeek;
+
+    public override bool CanWrite => stream.CanWrite;
+
+    public override long Length => stream.Length;
+
+    public override long Position
+    {
+        get => stream.Position;
+        set => stream.Position = value;
+    }
+}
---
+++ Decorator/Decorator.HeadFirst.Io/Program.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+
+namespace Decorator.HeadFirst.Io
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var faker = new Faker();
+            using var memoryStream = new MemoryStream();
+            using var stream = new LowerCaseInputStream(memoryStream);
+            using var streamWriter = new StreamWriter(stream);
+            for (var i = 0; i < 10; i++)
+            {
+                streamWriter.WriteLine(faker.Rant.Review());
+            }
+
+            streamWriter.Flush();
+            stream.Seek(0, SeekOrigin.Begin);
+
+            using var streamReader = new StreamReader(stream);
+            var data = streamReader.ReadToEnd();
+            Console.WriteLine(data);
+        }
+    }
+}
