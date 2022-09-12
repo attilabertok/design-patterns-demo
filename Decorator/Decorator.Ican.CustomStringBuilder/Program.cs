9a00a70e1c298fe80500dfc0ec10f9cb0e45318e
bat <hun.azazel@gmail.com>
2022-09-12 02:11:00 +0200

    Decorator.Ican.CustomStringBuilder

---
+++ Decorator/Decorator.Ican.CustomStringBuilder/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Decorator.Ican.CustomStringBuilder
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var cb = new CodeBuilder();
+
+            cb.AppendLine("class foo")
+                .AppendLine("{")
+                .AppendLine("}");
+
+            Console.WriteLine(cb.ToString());
+        }
+    }
+}
